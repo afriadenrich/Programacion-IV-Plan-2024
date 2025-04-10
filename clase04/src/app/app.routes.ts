8664d05aa7c6f0da7cf3f09e50da827047d816b1
AgustinFrich <agustinfrich@gmail.com>
2025-04-10 19:48:48 -0300

    Ejercicio Autos

---
+++ clase04/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AutosComponent } from './pages/autos/autos.component';
+
+export const routes: Routes = [{
+    path: "", component: AutosComponent
+}];
