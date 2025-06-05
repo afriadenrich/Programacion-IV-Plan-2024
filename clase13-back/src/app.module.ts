0442ab6ee0984dcf5fbb1b0cc6ba52eaa03934c5
AgustinFrich <agustinfrich@gmail.com>
2025-06-05 19:26:21 -0300

    Carpeta pública - disponibiliza imagenes

--- clase13-back/src/app.module.ts
+++ clase13-back/src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { EntidadModule } from './entidad/entidad.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'node:path';
 
 @Module({
-  imports: [UsuariosModule, EntidadModule, ConfigModule.forRoot()],
+  imports: [
+    UsuariosModule,
+    EntidadModule,
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
