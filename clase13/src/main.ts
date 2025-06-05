d61c390fa4e541714f7f94691d4c1ab0651de67a
AgustinFrich <agustinfrich@gmail.com>
2025-06-05 19:35:16 -0300

    creacion de carpeta de forma dinamica

--- clase13/src/main.ts
+++ clase13/src/main.ts
@@ -1,7 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { existsSync, mkdirSync } from 'node:fs';
 
 async function bootstrap() {
+  if (!existsSync('./public/images')) {
+    mkdirSync('./public/images');
+  }
+
   const app = await NestFactory.create(AppModule);
 
   console.log(process.env.FRONT_URL);
